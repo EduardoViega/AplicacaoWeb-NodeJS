d50ea31b6ad17fec99c70a02646de7d815f8f385
EduardoViega <eduardoviega12@gmail.com>
2022-06-19 14:24:05 -0300

    First Commit

--- modelos/carro.js
+++ modelos/carro.js
@@ -1,5 +1,6 @@
 var sequelize = require("sequelize")
 var banco = require("./../bancoConfig")
+var abastecimento = require("./abastecimento")
 
 var carro = banco.define("carro",{
     idCarro: {
@@ -17,7 +18,7 @@ var carro = banco.define("carro",{
         allowNull: false,
     },
     usado: {
-        type: sequelize.BOOLEAN,
+        type: sequelize.INTEGER,
         allowNull: false,
     }
 },{
@@ -25,4 +26,18 @@ var carro = banco.define("carro",{
     timestamps: false //Cria duas tabelas: quando foi criado e quando foi alterado por último
 })
 
-carro.sync() //Se não existir no banco ela é criada
+// Relação 1-1 (A.hasOne(B))
+abastecimento.hasOne(carro,{
+    foreignKey: "idAbastecimento"
+})
+carro.belongsTo(abastecimento,{
+    foreignKey: "idAbastecimento"
+})
+// Relação 1-X (A.belongsTo(B))
+// Relação X-X (A.belongsToMany(B, { through: 'C' }))
+
+
+//Se não existir no banco ela é criada, possui variações para atualizar
+// carro.sync()
+
+module.exports = carro
