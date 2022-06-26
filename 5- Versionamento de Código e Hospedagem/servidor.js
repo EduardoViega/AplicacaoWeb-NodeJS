0bbfc8a0c3915b4ac579fe7650a6b82ce98e9b1a
EduardoViega <eduardoviega12@gmail.com>
2022-06-26 17:04:23 -0300

    Projeto 5 primeito commit

---
+++ 5- Versionamento de Código e Hospedagem/servidor.js
@@ -0,0 +1,21 @@
+var express = require("express")
+var handlebars = require("express-handlebars")
+var rotas = require("./routes/carroRoutes")
+
+var servidor = express()
+const PORTA = 8082
+
+//configuração do handlebars no projeto
+servidor.engine("handlebars", handlebars({defaultLayout:"main"}))
+servidor.set("view engine","handlebars")
+
+//configurando rotas
+servidor.use(express.urlencoded({extended:true}))
+servidor.use(rotas)
+
+servidor.listen(PORTA,function(){
+    console.log("Executando servidor...")
+})
+
+
+
