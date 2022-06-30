755c275355f93bd8ba39225c1951a5a4056895fe
Eduardo Viega <eduardoviega12@gmail.com>
2022-06-30 17:04:19 -0300

    Projeto Final e readme 5

---
+++ 6- Trabalho Final/controllers/controladorProduto.js
@@ -0,0 +1,153 @@
+var produto = require("../models/produto")
+var axios = require("axios")
+var qs = require("querystring")
+
+var controlador = {}
+
+//Create - POST
+controlador.inserir = function(req,res){
+    produto.create({
+        descricao: req.body.descricao,
+        preco: req.body.preco
+    }).then(
+        function(dados){
+            res.status(200).redirect("/produtos")
+        }
+    ).catch(
+        function(erro){
+            res.status(500).send("Erro ao inserir o produto: "+erro)
+        }
+    )
+}
+
+//Read - GET
+controlador.buscar = function(req,res){
+    produto.findAll({
+        raw: true
+    }).then(
+        function(dados){
+            //res.status(200).send(dados)
+            res.render("tabela",{
+                produto: dados
+            }
+            )
+        }
+    ).catch(
+        function(erro){
+            res.status(500).send("Erro ao buscar por produtos: "+erro)
+        }
+    )
+}
+
+//Read - GET 2
+controlador.buscarUm = function(req,res){
+    produto.findAll({
+        raw: true,
+        where: {
+            idproduto: req.params.id
+        }
+    }).then(
+        function(dados){
+            res.status(200).send(dados)
+        }
+    ).catch(
+        function(erro){
+            res.status(500).send("Erro ao buscar por produto: "+erro)
+        }
+    )
+}
+
+//Update - PUT
+controlador.atualizar = function(req,res){
+    produto.update({
+        descricao: req.body.descricao,
+        preco: req.body.preco
+    },{
+        where:{
+            idproduto: req.params.id
+        }
+    }).then(
+        function(dados){
+            res.sendStatus(200)
+        }
+    ).catch(
+        function(erro){
+            res.status(500).send("Erro ao atualizar um produto: "+erro)
+        }
+    )
+}
+
+//Delete - DELETE
+controlador.remover = function(req,res){
+    produto.destroy({
+        where:{
+            idproduto: req.params.id
+        }
+    }).then(
+        function(dados){
+            res.sendStatus(200)
+        }
+    ).catch(
+        function(erro){
+            res.status(500).send("Erro ao remover um produto: "+erro)
+        }
+    )
+}
+
+
+
+//solicitarNovoFormulario
+controlador.novoFormulario = function(req,res){
+    res.render("novoForm")
+}
+
+//solicitarEditarFormulario
+controlador.editarFormulario = function(req,res){
+    res.render("editarForm",{
+        idproduto: req.params.id,
+        descricao: req.params.descricao,
+        preco: req.params.preco
+    })
+}
+
+//montarRequisiçãoEditar
+controlador.montarReqEdicao = function (req, res) {
+    axios.put("/produtos/" + req.params.id,
+        qs.stringify({
+            descricao: req.body.descricao,
+            preco: req.body.preco,
+        }),
+        {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            proxy:{
+                host: "15.228.157.227",
+                port: 3000
+            }
+        }
+    ).then(function () {
+            res.status(200).redirect("/produtos")
+        })
+        .catch(function (err) {
+            res.status(500).send("Erro ao editar o produto: " + err);
+        })
+}
+
+
+//montarRequisiçãoRemover
+controlador.montarReqDelete = function (req, res) {
+    axios.delete('/produtos/' + req.params.id,{
+        proxy:{
+            host: "15.228.157.227",
+            port: 3000
+        }
+    }).then(function () {
+            res.status(200).redirect("/produtos")
+        })
+        .catch(function (err) {
+            res.status(500).send("Erro ao apagar um produto: " + err);
+        })
+}
+
+module.exports = controlador
---
+++ 6- Trabalho Final/routes/produtoRoutes.js
@@ -0,0 +1,22 @@
+var express = require("express")
+var produtoControlador = require("../controllers/controladorProduto")
+
+var rotas = express.Router()
+
+//rotas da API
+rotas.post("/produtos",produtoControlador.inserir)
+rotas.get("/",produtoControlador.buscar)
+rotas.get("/produtos",produtoControlador.buscar)
+rotas.get("/produtos/:id",produtoControlador.buscarUm)
+rotas.put("/produtos/:id",produtoControlador.atualizar)
+rotas.delete("/produtos/:id",produtoControlador.remover)
+
+
+//rotas de páginas
+rotas.get("/cadastrar",produtoControlador.novoFormulario)   //retorna a página de cadastro
+rotas.get("/editar/:id/:descricao/:preco",produtoControlador.editarFormulario) //retorna a pagina de edição
+rotas.post("/ediReq/:id",produtoControlador.montarReqEdicao) //monta requisição de edição
+rotas.get("/remover/:id",produtoControlador.montarReqDelete)  //monta requisição de remoção
+
+
+module.exports = rotas
